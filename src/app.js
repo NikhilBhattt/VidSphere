7a369a757edb40b886ab7c4e128bbc7e4d385e7f
Nikhil-Bhatt <nikhilbhatt220705@gmail.com>
2026-04-15 00:44:59 +0530

    add express-rate-limit middleware and update dependencies

--- src/app.js
+++ src/app.js
@@ -3,10 +3,19 @@ dotenv.config();
 
 import express from "express";
 import cors from "cors";
+import { rateLimit } from "express-rate-limit";
 import cookieParser from "cookie-parser";
 import { router as userRouter } from "../routes/user.routes.js";
 
 const app = express();
+
+const limiter = rateLimit({
+  limit: 100,
+  windowMs: 60 * 60 * 1000, // 1 hour
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+
+app.use(limiter);
 
 app.use(
   cors({
